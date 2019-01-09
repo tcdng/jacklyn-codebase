7c541af3d16e0889cce0b13c8364958800c815d9
Lateef Ojulari <lateefojulari@gmail.com>
2019-01-09 02:00:30 +0100

    Add statistics module

---
+++ jacklyn-statistics/src/main/java/com/tcdng/jacklyn/statistics/business/AbstractQuickPercentageProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 The Code Department
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.tcdng.jacklyn.statistics.business;
+
+import com.tcdng.jacklyn.statistics.data.QuickPercentage;
+import com.tcdng.unify.core.AbstractUnifyComponent;
+import com.tcdng.unify.core.UnifyException;
+
+/**
+ * Abstract base class for quick percentage providers.
+ * 
+ * @author Lateef Ojulari
+ * @since 1.0
+ */
+public abstract class AbstractQuickPercentageProvider<T extends QuickPercentage<?>> extends AbstractUnifyComponent
+        implements QuickPercentageProvider<T> {
+
+    @Override
+    protected void onInitialize() throws UnifyException {
+
+    }
+
+    @Override
+    protected void onTerminate() throws UnifyException {
+
+    }
+
+}
