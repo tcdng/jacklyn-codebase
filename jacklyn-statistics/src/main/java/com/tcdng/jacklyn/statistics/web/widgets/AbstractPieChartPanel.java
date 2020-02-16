82ddd455d36481d9702806d1157afb5dd37f5838
Lateef Ojulari <lateef.ojulari@tcdng.com>
2020-02-16 01:33:06 +0100

    Add common base pie chart panel

---
+++ jacklyn-statistics/src/main/java/com/tcdng/jacklyn/statistics/web/widgets/AbstractPieChartPanel.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018-2020 The Code Department
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.tcdng.jacklyn.statistics.web.widgets;
+
+import com.tcdng.jacklyn.common.web.widgets.BasePanel;
+import com.tcdng.jacklyn.statistics.business.QuickRatioVisualProvider;
+import com.tcdng.jacklyn.statistics.data.QuickRatioVisual;
+import com.tcdng.unify.core.UnifyException;
+import com.tcdng.unify.core.annotation.UplAttribute;
+import com.tcdng.unify.core.annotation.UplAttributes;
+import com.tcdng.unify.core.annotation.UplBinding;
+
+/**
+ * Convenient abstract base class for pie chart panels.
+ * 
+ * @author Lateef Ojulari
+ * @since 1.0
+ */
+@UplBinding("web/statistics/upl/piechartpanel.upl")
+@UplAttributes(@UplAttribute(name = "chartProvider", type = String.class, mandatory = true))
+public abstract class AbstractPieChartPanel extends BasePanel {
+
+    private QuickRatioVisual quickRatioVisual;
+
+    @Override
+    public void onPageConstruct() throws UnifyException {
+        super.onPageConstruct();
+        setComponentValueBeanToThis("chartImg");
+    }
+
+    @Override
+    public void switchState() throws UnifyException {
+        super.switchState();
+
+        QuickRatioVisualProvider provider = (QuickRatioVisualProvider) getComponent(
+                getUplAttribute(String.class, "chartProvider"));
+        quickRatioVisual = provider.provide(getValue());
+    }
+
+    public QuickRatioVisual getQuickRatioVisual() {
+        return quickRatioVisual;
+    }
+}
